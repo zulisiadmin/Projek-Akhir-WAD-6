9f8f1887b3f86c61b3e2701611de53663b07a202
Zuli Priadi <118255359+zulisiadmin@users.noreply.github.com>
2025-09-28 11:20:42 +0700

    umkm-ku nih

---
+++ umkm-ku/src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
